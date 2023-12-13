48bb34eeac7bf04d87decc87438e50f1c5937692
Sailynx-Zenefer <siderealism.space@gmail.com>
2023-12-13 16:30:53 +0000

    badmerge fix

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,9 @@ import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
-);
+)
